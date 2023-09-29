f1cf4f04910a587b4d8992109ee8b8bb4c228ef9
Omar Ashraf <oashraf304@gmail.com>
2023-09-29 04:56:32 +0300

    modified

--- src/Components/testimonials/Testimonials.jsx
+++ src/Components/testimonials/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
       name: "Tom Durden",
       title: "Backend Developer",
       img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "/src/assets/twitter.png",
+      icon: "../../../src/assets/twitter.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
     },
     {
@@ -15,7 +15,7 @@ const Testimonials = () => {
       name: "Alex Kalinski",
       title: "UI/UX Designer",
       img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "/src/assets/youtube.png",
+      icon: "../../../src/assets/youtube.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
       featured: true,
     },
@@ -24,7 +24,7 @@ const Testimonials = () => {
       name: "Martin Harold",
       title: "Web Developer",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "/src/assets/linkedin.png",
+      icon: "../../../src/assets/linkedin.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
     },
   ];
@@ -35,7 +35,11 @@ const Testimonials = () => {
         {data.map((d) => (
           <div className={d.featured ? "card featured" : "card"} key={d.id}>
             <div className="top">
-              <img src="/src/assets/right-arrow.png" className="left" alt="" />
+              <img
+                src="../../../src/assets/right-arrow.png"
+                className="left"
+                alt=""
+              />
               <img className="user" src={d.img} alt="" />
               <img className="right" src={d.icon} alt="" />
             </div>
